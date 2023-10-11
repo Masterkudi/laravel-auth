083209f2b6b4676d4b4745027a7450a2e5c84278
masterkudi <cudini.andrea@gmail.com>
2023-10-11 10:42:35 +0200

    provo a sostituire il form index con delle card

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -21,15 +21,22 @@
 
             <tbody>
                 @foreach ($projects as $project)
+                    {{--
                     <tr>
                         <td>{{ $project->title }}</td>
-                        <td><img src={{ asset('/storage/' . $project->image) }} class="img-thumbnail" style="width: 70px" ></td>
+                        <td><img src={{ asset('/storage/' . $project->image) }} class="img-thumbnail" style="width: 70px">
+                        </td>
                         <td>{{ $project->published_at?->format('d/m/Y H:i') }}</td>
-                        <td>
-                            <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-info"><i class="fa-solid fa-circle-info"></i></a>
-                            <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
-
-                            {{-- form per il delete --}}
+                        <td class=" text-center">
+                            <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-info"><i
+                                    class="fa-solid fa-circle-info"></i></a>
+                        </td>
+                        <td class=" text-center">
+                            <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-warning"><i
+                                    class="fa-solid fa-pen-to-square"></i></a>
+                        </td>
+                        <td class=" text-center">
+                            {{-- form per il delete 
                             <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
                                 @csrf()
                                 @method('DELETE')
@@ -37,6 +44,37 @@
                             </form>
                         </td>
                     </tr>
+                    --}}
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="card p-2">
+                                <img src="{{ asset('/storage/' . $project->image) }} class="img-thumbnail" style="width: 70px">
+                                <div class="card-body bg-white text-center">
+                                    <a href="{{ route('admin.projects.show', $project->slug) }}"></a>
+                                    <h5 class="card-title">
+                                        <a>{{ $project->title }}</a>
+                                    </h5>
+                                </div>
+                                <div class="d-flex">
+                                    <button class=" text-center">
+                                        <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-info"><i
+                                                class="fa-solid fa-circle-info"></i></a>
+                                    </button>
+                                    <button class=" text-center">
+                                        <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-warning"><i
+                                                class="fa-solid fa-pen-to-square"></i></a>
+                                    </button>
+                                    <button class=" text-center">
+                                        <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                                            @csrf()
+                                            @method('DELETE')
+                                            <button class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                                        </form>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             </tbody>
         </table>
